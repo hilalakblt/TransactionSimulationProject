fdfb6d22e1f67d57bd34b8181605b12aec5c7d10
hilal <akbuluthilal18@gmail.com>
2021-04-26 13:09:01 +0300

    'initial'

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUserA = new System.Windows.Forms.TextBox();
+            this.txtUserB = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of User A:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(96, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number of User B:";
+            // 
+            // txtUserA
+            // 
+            this.txtUserA.Location = new System.Drawing.Point(228, 80);
+            this.txtUserA.Name = "txtUserA";
+            this.txtUserA.Size = new System.Drawing.Size(100, 20);
+            this.txtUserA.TabIndex = 2;
+            // 
+            // txtUserB
+            // 
+            this.txtUserB.Location = new System.Drawing.Point(228, 140);
+            this.txtUserB.Name = "txtUserB";
+            this.txtUserB.Size = new System.Drawing.Size(100, 20);
+            this.txtUserB.TabIndex = 3;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(253, 219);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 302);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtUserB);
+            this.Controls.Add(this.txtUserA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUserA;
+        private System.Windows.Forms.TextBox txtUserB;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Threading;
+using System.IO;
+
+namespace Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private static void WriteFile(int UsersDeadlock, int DeadlocksTime, String UserType)
+        {
+            
+            String FilePath = @"C:\Users\medine\Desktop\Records.txt";
+            FileStream fileForRecords = new FileStream(FilePath, FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fileForRecords);
+            
+            sw.WriteLine(UserType);
+            sw.WriteLine(UsersDeadlock);
+            sw.WriteLine(DeadlocksTime);
+            sw.Flush();
+            sw.Close();
+            fileForRecords.Close();
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            int numberFromA = Convert.ToInt32(txtUserA.Text);
+            int numberFromB = Convert.ToInt32(txtUserB.Text);
+
+            for(int i = 0; i < numberFromA; i++)
+            {
+                Thread threadForA = new Thread(UserThreadA);
+                threadForA.Start();
+            }
+            
+            for (int i = 0; i < numberFromB; i++)
+            {
+                Thread threadForB = new Thread(UserThreadB);
+                threadForB.Start();
+            }
+            
+        }
+        
+        public void UserThreadA()
+        {
+            SqlConnection conForDatabase = new SqlConnection("Data Source=DESKTOP-S705IE6\\SQL2017;Initial Catalog=AdventureWorks2012;Integrated Security=True");
+            SqlTransaction transactionForSql = null;
+            SqlCommand command = new SqlCommand();
+
+            DateTime beginTime = DateTime.Now;
+            int UserADeadLock = 0;
+            TimeSpan totalTime;
+
+            for(int i=0; i<100; i++)
+            {
+                try
+                {
+                    conForDatabase.Open();
+                    command.Connection = conForDatabase;
+                    transactionForSql = conForDatabase.BeginTransaction(IsolationLevel.ReadUncommitted);
+
+                    command.CommandText = ("UPDATE Sales.SalesOrderDetail " +
+                            "SET UnitPrice = UnitPrice * 10.0 / 10.0 WHERE UnitPrice > " +
+                            "100AND EXISTS(SELECT* FROM Sales.SalesOrderHeader WHERE Sales.SalesOrderHeader.SalesOrderID = " +
+                            "Sales.SalesOrderDetail.SalesOrderID AND Sales.SalesOrderHeader.OrderDate BETWEEN" +
+                            " @BeginDate AND @EndDate AND Sales.SalesOrderHeader.OnlineOrderFlag = 1)");
+                    command.Transaction = transactionForSql;
+
+                    Random random = new Random();
+
+
+                    if (random.NextDouble() < 0.5)
+                    {
+                        command.Parameters.AddWithValue("@BeginDate", 20110101);
+                        command.Parameters.AddWithValue("@EndDate", 20111231);
+                        command.ExecuteNonQuery();
+                    }
+                    if (random.NextDouble() < 0.5)
+                    {
+                        
+                        command.Parameters.AddWithValue("@BeginDate", 20120101);
+                        command.Parameters.AddWithValue("@EndDate", 20121231);
+                        command.ExecuteNonQuery();
+                    }
+                    if (random.NextDouble() < 0.5)
+                    {
+                        command.Parameters.AddWithValue("@BeginDate", 20130101);
+                        command.Parameters.AddWithValue("@EndDate", 20131231);
+                        command.ExecuteNonQuery();
+                    }
+                    if (random.NextDouble() < 0.5)
+                    {
+                        command.Parameters.AddWithValue("@BeginDate", 20140101);
+                        command.Parameters.AddWithValue("@EndDate", 20141231);
+                        command.ExecuteNonQuery();
+                    }
+                    if (random.NextDouble() < 0.5)
+                    {
+                        command.Parameters.AddWithValue("@BeginDate", 20150101);
+                        command.Parameters.AddWithValue("@EndDate", 20151231);
+                        command.ExecuteNonQuery();
+                    }
+
+                    transactionForSql.Commit();
+                    conForDatabase.Close();
+                    SqlConnection.ClearPool(conForDatabase);
+
+                }
+                catch(SqlException exception)
+                {
+                    UserADeadLock++;
+                    transactionForSql.Rollback();
+                }
+                finally
+                {
+                    if (conForDatabase.State == ConnectionState.Open)
+                    {
+                        conForDatabase.Close();
+                        SqlConnection.ClearPool(conForDatabase);
+                    }
+                }
+            }
+            DateTime endTime = DateTime.Now;
+            totalTime = endTime - beginTime;
+            int totalSecond = Convert.ToInt32(totalTime.TotalSeconds);
+
+            //WriteFile(UserADeadLock, totalSecond, "User A");
+            Console.WriteLine("User A's Deadlocks are: {0}", UserADeadLock);
+            Console.WriteLine("User A's Total Time is: {0}", totalTime);
+
+            if (conForDatabase.State == ConnectionState.Open)
+                conForDatabase.Close();
+        }
+        public void UserThreadB()
+        {
+            SqlConnection conForDatabase = new SqlConnection("Data Source=DESKTOP-S705IE6\\SQL2017;Initial Catalog=AdventureWorks2012;Integrated Security=True");
+            SqlTransaction transactionForSql = null;
+            SqlCommand command = new SqlCommand();
+
+            DateTime beginTime = DateTime.Now;
+            int UserBDeadLock = 0;
+            TimeSpan totalTime;
+
+            for (int i = 0; i < 100; i++)
+            {
+                try
+                {
+                    
+                    conForDatabase.Open();
+                    command.Connection = conForDatabase;
+                    transactionForSql = conForDatabase.BeginTransaction(IsolationLevel.ReadUncommitted);
+                    command.Transaction = transactionForSql;
+
+
+
+                    command.CommandText = ("SELECT SUM(Sales.SalesOrderDetail.OrderQty) FROM Sales.SalesOrderDetail " +
+                            "WHERE UnitPrice > 100 AND EXISTS(SELECT* FROM Sales.SalesOrderHeader WHERE " +
+                            "Sales.SalesOrderHeader.SalesOrderID = Sales.SalesOrderDetail.SalesOrderID AND " +
+                            "Sales.SalesOrderHeader.OrderDate BETWEEN @BeginDate AND @EndDate AND Sales.SalesOrderHeader.OnlineOrderFlag = 1)");
+
+                    Random random = new Random();
+
+                    if (random.NextDouble() < 0.5)
+                    {
+                        
+                        command.Parameters.AddWithValue("@BeginDate", 20110101);
+                        command.Parameters.AddWithValue("@EndDate", 20111231);
+                        command.ExecuteNonQuery();
+                    }
+
+                    if (random.NextDouble() < 0.5)
+                    {
+                        command.Parameters.AddWithValue("@BeginDate", 20120101);
+                        command.Parameters.AddWithValue("@EndDate", 20121231);
+                        command.ExecuteNonQuery();
+                    }
+                    if (random.NextDouble() < 0.5)
+                    {
+                        command.Parameters.AddWithValue("@BeginDate", 20130101);
+                        command.Parameters.AddWithValue("@EndDate", 20131231);
+                        command.ExecuteNonQuery();
+                    }
+                    if (random.NextDouble() < 0.5)
+                    {
+                        command.Parameters.AddWithValue("@BeginDate", 20140101);
+                        command.Parameters.AddWithValue("@EndDate", 20141231);
+                        command.ExecuteNonQuery();
+                    }
+                    if (random.NextDouble() < 0.5)
+                    {
+                        command.Parameters.AddWithValue("@BeginDate", 20150101);
+                        command.Parameters.AddWithValue("@EndDate", 20151231);
+                        command.ExecuteNonQuery();
+                    }
+
+                    transactionForSql.Commit();
+                    conForDatabase.Close();
+                    SqlConnection.ClearPool(conForDatabase);
+
+                }
+                catch (SqlException exception)
+                {
+                    UserBDeadLock++;
+                    transactionForSql.Rollback();
+                }
+                finally
+                {
+                    if (conForDatabase.State == ConnectionState.Open)
+                    {
+                        conForDatabase.Close();
+                        SqlConnection.ClearPool(conForDatabase);
+                    }
+
+                }
+            }
+            DateTime endTime = DateTime.Now;
+            totalTime = endTime - beginTime;
+            int totalSecond = Convert.ToInt32(totalTime.TotalSeconds);
+
+            //WriteFile(UserBDeadLock, totalSecond, "User B");
+            Console.WriteLine("User B's Deadlocks are: {0}", UserBDeadLock);
+            Console.WriteLine("User B's Total Time is: {0}", totalTime);
+
+        }
+    }
+}
